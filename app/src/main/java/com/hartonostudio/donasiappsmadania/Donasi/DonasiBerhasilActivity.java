e3434c613942a6c7e093311cc38d25184356e1f1
Rachmad Hartono <rachmad.hartono@students.amikom.ac.id>
2021-04-17 12:02:13 +0700

    comit ke 1

---
+++ app/src/main/java/com/hartonostudio/donasiappsmadania/Donasi/DonasiBerhasilActivity.java
@@ -0,0 +1,58 @@
+package com.hartonostudio.donasiappsmadania.Donasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hartonostudio.donasiappsmadania.HomeActivity;
+import com.hartonostudio.donasiappsmadania.R;
+
+public class DonasiBerhasilActivity extends AppCompatActivity {
+
+    Animation app_splash, btt, ttb;
+    Button btn_home;
+    TextView app_title, app_subtitle;
+    ImageView icon_succes_ticket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donasi_berhasil);
+
+        btn_home = findViewById(R.id.btn_home);
+        app_title = findViewById(R.id.app_title);
+        app_subtitle = findViewById(R.id.app_subtitle);
+        icon_succes_ticket = findViewById(R.id.icon_succes_ticket);
+
+        //Load Animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+
+        //Run Animation
+        icon_succes_ticket.startAnimation(app_splash);
+
+        app_title.startAnimation(ttb);
+        app_subtitle.startAnimation(ttb);
+
+        btn_home.startAnimation(btt);
+
+        //berpindah Activity MyProfile
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotohome = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(gotohome);
+            }
+        });
+
+
+    }
+}
