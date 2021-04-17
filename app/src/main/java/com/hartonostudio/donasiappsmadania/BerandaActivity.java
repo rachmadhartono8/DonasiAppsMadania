aaf5cdac7b60fb41d559eedaade2f18b042de0d6
Rachmad Hartono <rachmad.hartono@students.amikom.ac.id>
2021-04-17 13:16:13 +0700

    comit ke 1

---
+++ app/src/main/java/com/hartonostudio/donasiappsmadania/BerandaActivity.java
@@ -0,0 +1,14 @@
+package com.hartonostudio.donasiappsmadania;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BerandaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+    }
+}
